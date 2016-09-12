1f6441ad6269842c93294977e4d5cf8ec16738dd
Dorian Kersch <Dorian_Kersch@intuit.com>
2016-09-11 23:51:33 -0700

    created sendToJira and modified sendToSlack to handle keys from index.js

--- src/processor/JiraWebHookProcessor.js
+++ src/processor/JiraWebHookProcessor.js
@@ -16,46 +16,47 @@ module.exports = class JiraWebHookProcessor {
   }
 
   /**
-   * Converts a Jira WebHook notification into a a message that's human readable
-   *
-   * @param {object} body - Jira WebHook post request
-   * @returns {string} notification message
-   */
-  getImTextFromBody(body) {
-    jiraFormatter.setUrl(this.config.jiraUrl);
-    jiraFormatter.setData(body);
-
-    let text;
-    switch (body.webhookEvent) {
-      case 'jira:issue_created':
-        text = jiraFormatter.getCreated();
-        break;
-      case 'jira:issue_updated':
-        text = jiraFormatter.getUpdatedDesc() ||
-          jiraFormatter.getStatusChangeWithComment() ||
-          jiraFormatter.getStatusChange() ||
-          jiraFormatter.getComment();
-        break;
-      case 'jira:issue_deleted':
-        text = jiraFormatter.getDeleted();
-        break;
-    }
-
-    return text;
-  }
-
-  /**
-   * Takes a Jira WebHook requests, converts it and sends it to Slack
+   * Given a response. Comments on JIRA tickets
    *
    * @param {object} body - body from Express.js
    */
-  sendToSlack(body) {
-    if (!this.config.projects.hasOwnProperty(body.issue.fields.project.key)) {
-      throw new Error(`Unkown project: ${body.issue.fields.project.key}`);
-    }
+   sendToJira(body, key){
+     let jiraURL = this.config.jiraUrl;
+     let jiraCommentAPI = '/rest/api/2/issue/';
+     let payload = {"body": body};
+     let auth = ''
+     const uname = this.config.username;
+     const pword = this.config.password;
+     if (uname != '' && pword != ''){
+       auth = "Basic " + new Buffer(uname + ':' + pword).toString("base64");
+     }
+     if (body && auth && key) {
+       this.httpClient.post({
+         url: jiraURL + jiraCommentAPI + key + '/comment',
+         json: payload,
+         headers: {
+           "Authorization" : auth
+         }
+   }, (error, response, body) => {
+     if (error) {
+       throw new Error(body);
+     }
+   });
+ }
+ }
 
-    let projectConfig = this.config.projects[body.issue.fields.project.key];
-    projectConfig.payload.text = this.getImTextFromBody(body);
+  /**
+   * Takes a message and sends it to Slack
+   *
+   * @param {object} body - body from Express.js
+   * @param {string} key - jira project key to match to configuration
+   */
+  sendToSlack(body, key) {
+    if (!this.config.projects.hasOwnProperty(key)) {
+      throw new Error(`Unkown project: ${key}`);
+    }
+    let projectConfig = this.config.projects[key];
+    projectConfig.payload.text = body;
 
     if (projectConfig.payload.text) {
       this.httpClient.post({
